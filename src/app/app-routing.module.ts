bd60635fbf007dbf83bcb8324a881132436e1e50
Charu Kush <charu@Amits-MacBook-Pro-2.local>
2020-09-12 16:33:25 -0400

    Blog Routing between pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
+import { NewpostComponent } from './newpost/newpost.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'myaccount', component:MyaccountComponent},
+  {path: 'newpost', component:NewpostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
